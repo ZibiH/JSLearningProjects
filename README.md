800bb0b3ddf29b4003a3ed12f7a1d32dcf3c7d08
ZibiH <zbigniew.holak@gmail.com>
2021-05-27 14:45:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### This is a public repository of my projects created mainly for JavaScript learning purposes
+
+\*\* More info soon (I'm going to add some description to every project/branch as soon as created)
---
+++ app.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('button');
+
+function randomizeColor() {
+	const redSaturation = Math.floor(Math.random() * 255);
+	const greenSaturation = Math.floor(Math.random() * 255);
+	const blueSaturation = Math.floor(Math.random() * 255);
+	const randomRgbaColor = `rgba(${redSaturation}, ${greenSaturation}, ${blueSaturation}, 1)`;
+	console.log(randomRgbaColor);
+	return randomRgbaColor;
+}
+
+function changeBackgroundColor() {
+	document.body.style.backgroundColor = randomizeColor();
+}
+
+button.addEventListener('click', changeBackgroundColor);
